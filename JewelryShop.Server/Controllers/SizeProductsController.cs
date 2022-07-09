b6dcb17cedc9c93835feb935879753a90dfbe072
shongrin <shongrin@gmail.com>
2022-07-09 21:57:22 +0300

    Feat added supplier  and sizeProducts repositoies

---
+++ JewelryShop.Server/Controllers/SizeProductsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JewelryShop.Data;
+using JewelryShop.Data.Models;
+using JewelryShop.Data.Repository.Interfaces;
+
+namespace JewelryShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SizeProductsController : ControllerBase
+    {
+        private readonly ISizeProductRepository sizeProductRepository;
+
+        public SizeProductsController(ISizeProductRepository sizeProductRepository) => this.sizeProductRepository = sizeProductRepository;
+
+        // GET: api/SizeProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SizeProduct>>> GetSizeProducts() => Ok(await sizeProductRepository.GetAll());
+
+        // GET: api/SizeProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SizeProduct>> GetSizeProduct(int id)
+        {
+            try
+            {
+                return await sizeProductRepository.Get(id);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        // PUT: api/SizeProducts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSizeProduct(int id, SizeProduct sizeProduct)
+        {
+            if (id != sizeProduct.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await sizeProductRepository.Update(sizeProduct);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/SizeProducts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SizeProduct>> PostSizeProduct(SizeProduct sizeProduct)
+        {
+            try
+            {
+                return await sizeProductRepository.Insert(sizeProduct);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        // DELETE: api/SizeProducts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSizeProduct(int id)
+        {
+            try
+            {
+                await sizeProductRepository.Delete(id);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return NotFound();
+        }
+
+    }
+}
---
+++ JewelryShop.Server/Controllers/SuppliersController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JewelryShop.Data;
+using JewelryShop.Data.Models;
+using JewelryShop.Data.Repository.Interfaces;
+
+namespace JewelryShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ISupplierRepository supplierRepository;
+
+        public SuppliersController(ISupplierRepository supplierRepository)
+        {
+            this.supplierRepository = supplierRepository;
+        }
+
+        // GET: api/Suppliers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers() => Ok(await supplierRepository.GetAll());
+        // GET: api/Suppliers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            try
+            {
+                return await supplierRepository.Get(id);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        // PUT: api/Suppliers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier(int id, Supplier supplier)
+        {
+            if (id != supplier.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await supplierRepository.Update(supplier);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Suppliers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            try
+            {
+                return await supplierRepository.Insert(supplier);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        // DELETE: api/Suppliers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSupplier(int id)
+        {
+            try
+            {
+                await supplierRepository.Delete(id);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return NotFound();
+        }
+
+    }
+}
